baa735429c75cd17080d85f0d79ad91e45ac8636
Denis Ivanov <ha150685idj@gmail.com>
2021-10-15 11:00:30 +0300

    init commit

--- src/com/pb/ivanov/hw2/Interval.java
+++ src/com/pb/ivanov/hw2/Interval.java
@@ -1,22 +1,27 @@
 package com.pb.ivanov.hw2;
 
+import java.util.Scanner;
+
 public class Interval {
     public static void main(String[] args) {
 
-//        if (sign == '+'){
-//            result= operand1 + operand2;
-//        } else if (sign == '-'){
-//            result= operand1 - operand2;
-//        } else if (sign == '*') {
-//            result = operand1 * operand2;
-//        } else if (sign == '/') {
-//            if (operand2 == 0) {
-//                System.out.print("На ноль делить нельзя!");
-//            } else {
-//                result = operand1 / operand2;
-//            }
-//        }
-//
-//        System.out.print("Результат: " + result);
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите число от 0 до 100: ");
+        int number = in.nextInt();
+
+        if (number < 0) {
+            System.out.println("Ошибка! Число " + number + " не в промежутке [0-100]");
+        } else if (number > 100){
+            System.out.println("Ошибка! Число " + number + " не в промежутке [0-100]");
+        } else if (number <= 14){
+            System.out.println("Число " + number + " в промежутке [0-14]");
+        } else if (number <= 35){
+            System.out.println("Число " + number + " в промежутке [15-35]");
+        } else if (number <= 50){
+            System.out.println("Число " + number + " в промежутке [36-50]");
+        } else if (number <= 100){
+            System.out.println("Число " + number + " в промежутке [51-100]");
+        }
     }
 }
