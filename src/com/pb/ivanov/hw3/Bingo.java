ee93e89e76e15b2f120cfb0194f2d50d7599a206
Denis Ivanov <ha150685idj@gmail.com>
2021-10-21 23:22:22 +0300

    init commit

---
+++ src/com/pb/ivanov/hw3/Bingo.java
@@ -0,0 +1,40 @@
+package com.pb.ivanov.hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        System.out.println("Угадайте число от 0 до 100. Количество попыток не ограничено.");
+        System.out.println("Для выхода из программы введите - 101.");
+        Random number = new Random();
+        int x = number.nextInt(101);
+        //System.out.println("rand x: " + x); // раскомментировать для проверки работы алгоритма
+
+        Scanner in = new Scanner(System.in);
+
+
+        int attempt = 0;
+        int infinity = Integer.MAX_VALUE;
+
+        while (attempt < infinity) {
+            attempt++;
+            System.out.print("Введите число: ");
+            int usersNumber = in.nextInt();
+
+            if (usersNumber == 101) {
+                break;
+            }
+            if (usersNumber < x) {
+                System.out.print("Загаданное число больше " + usersNumber + "! ");
+                continue;
+            } else if (usersNumber > x) {
+                System.out.print("Загаданное число меньше " + usersNumber+ "! ");
+                continue;
+            }
+            System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
+            break;
+        }
+        System.out.println("Игра окончена!");
+    }
+}
