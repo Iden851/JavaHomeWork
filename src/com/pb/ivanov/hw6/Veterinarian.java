66d13ee8fe86a33c7257092c0715b56e162e1fb3
Denis Ivanov <ha150685idj@gmail.com>
2021-11-06 23:39:17 +0200

    init commit

--- src/com/pb/ivanov/hw6/Veterinarian.java
+++ src/com/pb/ivanov/hw6/Veterinarian.java
@@ -4,6 +4,8 @@ public class Veterinarian extends Animal {
 
 
     public void treatAnimal(Animal animal){
-        System.out.println("На приеме ветеринара: " + animal + '\n' + "Питание: " + animal.getFood() + '\n' + "Город: " + animal.getLocation());
+        System.out.println("На приеме ветеринара: " + animal + '\n' +
+                "Питание: " + animal.getFood() + '\n' +
+                "Город: " + animal.getLocation());
     }
 }
